72cf95f0ec18ba705f4df3451fe3efc8542041f0
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2023-07-19 23:18:35 -0700

    Adjustments

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ import json
 import boto3
 import logging
 from botocore.exceptions import NoCredentialsError
-
 import json_builder.bin.kriya_object as kriya_object
-
 
 polly = boto3.client(
     'polly',
-    region_name='us-west-2',  # Add this line to specify the region
+    region_name='us-west-2',
 )
 
 s3 = boto3.client(
@@ -54,17 +52,15 @@ def create_hello_world_audio():
 def json_to_audio(jsondata, bucket_name, s3_key):
     kriya_obj = kriya_object.create_kriya_obj_from_json(jsondata)
     filename = kriya_obj.title.replace(".json", "")
-
-    # Generate the audio using Amazon Polly
     audio_data = synthesize_speech(filename)
-
-    # Upload the generated audio to Amazon S3
     success = upload_to_s3(bucket_name, s3_key, audio_data)
-
     return success
 
-# Initialize Flask app
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.route('/bambi-tts-api', methods=['POST'])
 def handle_tts_request():
@@ -72,18 +68,15 @@ def handle_tts_request():
     jsondata = {}
     print("POST received")
     try:
-        jsondata = request.get_json()  # get the JSON object from the request
+        jsondata = request.get_json()
         print("Received data: ", jsondata)
-
         bucket_name = 'crystal-audio-processing'
         s3_key = 'audio-dumps/audio-gen-files/crystal_demo.wav'
         success = json_to_audio(jsondata, bucket_name, s3_key)
-
         if success:
             return jsonify({'message': 'Audio file created successfully'}), 200
         else:
             return jsonify({'message': 'Error occurred while creating audio file'}), 500
-
     except Exception as e:
         print("Error: ", str(e))
         return {"success": False},
